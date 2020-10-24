b535def26c74b80e439d2a672479165e1d0a731f
Admin <jakedean202@gmail.com>
2020-10-24 23:26:13 +0100

    Added Message and a respective MessageEncoder
    A Message is a self-contained wrapper for content sent via the Bridge, and the MessageEncoder works to allow for translating Java objects to packets

---
+++ src/main/java/comms/MessageEncoder.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) Jake Dean, 2020.
+ *
+ * This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/4.0/ or send a letter to
+ * Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+ */
+
+package comms;
+
+import ent.Entity;
+
+import java.util.UUID;
+
+/*
+Encodes information for outbound transfers.
+ */
+public class MessageEncoder {
+    private final Message.State state = Message.State.OUTBOUND;
+    private String message;
+    private UUID responseCode;
+
+    public MessageEncoder(String message) {
+        this.message = message;
+    }
+
+    public MessageEncoder(Entity entity) {
+        setMessage(entity);
+    }
+
+    public Message.State getState() {
+        return state;
+    }
+
+    public MessageEncoder setResponseCode(UUID responseCode) {
+        this.responseCode = responseCode;
+        return this;
+    }
+
+    public MessageEncoder setMessage(String input) {
+        //TODO
+        return this;
+    }
+
+    public MessageEncoder setMessage(Entity input) {
+        //TODO
+        return this;
+    }
+
+    public Message encode() {
+        return new Message(message, responseCode, state);
+    }
+
+}
